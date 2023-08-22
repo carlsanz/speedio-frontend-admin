e19a1a7d982d58dfd519eb25e5cf880d5c57f034
unknown <carlsanz_2013@yahoo.com>
2023-08-22 00:33:38 -0600

    Creacion de pagina para el perfil

--- js/controlador.js
+++ js/controlador.js
@@ -1,14 +1,17 @@
 
 function inicio() {
-  document.getElementById("inicio-sesion").style.display="block"
-  document.getElementById("inicio").style.display="none"
-  document.getElementById("inicio-empleados").style.display= "none"
-  document.getElementById("pedidos-sin-tomar").style.display= "none"
-  document.getElementById("empresas").style.display= "none"
-  document.getElementById("agregar-empresa").style.display= "none"
-  document.getElementById("productos").style.display= "none"
+  document.getElementById("inicio-sesion").style.display="block";
+  document.getElementById("inicio").style.display="none";
+  document.getElementById("inicio-empleados").style.display= "none";
+  document.getElementById("pedidos-sin-tomar").style.display= "none";
+  document.getElementById("empresas").style.display= "none";
+  document.getElementById("agregar-empresa").style.display= "none";
+  document.getElementById("productos").style.display= "none";
+  document.getElementById("productos-Disponibles").style.display= "none";
+  document.getElementById("agregar-Productos").style.display= "none";
+  document.getElementById("perfil").style.display= "none";
 }
-// inicio();
+inicio();
 
 function validarFormulario() {
     const emailInput = document.getElementById('email');
@@ -39,39 +42,48 @@ function validarFormulario() {
   function ingreso() {
     document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
     
-    document.getElementById("inicio-sesion").style.display="none"
-    document.getElementById("inicio").style.display="block"
-    document.getElementById("inicio-empleados").style.display= "none"
-    document.getElementById("pedidos-sin-tomar").style.display= "none"
-    document.getElementById("empresas").style.display= "none"
-    document.getElementById("agregar-empresa").style.display= "none"
-    document.getElementById("productos").style.display= "none"
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="block";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "none";
 
   }
 
   function repartidores() {
     document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
    
-    document.getElementById("inicio-sesion").style.display="none"
-    document.getElementById("inicio").style.display="none"
-    document.getElementById("inicio-empleados").style.display= "block"
-    document.getElementById("pedidos-sin-tomar").style.display= "none"
-    document.getElementById("empresas").style.display= "none"
-    document.getElementById("agregar-empresa").style.display= "none"
-    document.getElementById("productos").style.display= "none"
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "block";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "none";
 
   }
 
   function asignarPedido() {
     document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
     
-    document.getElementById("inicio-sesion").style.display="none"
-    document.getElementById("inicio").style.display="none"
-    document.getElementById("inicio-empleados").style.display= "none"
-    document.getElementById("pedidos-sin-tomar").style.display= "block"
-    document.getElementById("empresas").style.display= "none"
-    document.getElementById("agregar-empresa").style.display= "none"
-    document.getElementById("productos").style.display= "none"
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "block";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "none";
 
 
   }
@@ -79,36 +91,90 @@ function validarFormulario() {
   function generarPantallaEmpresa() {
     document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
     
-    document.getElementById("inicio-sesion").style.display="none"
-    document.getElementById("inicio").style.display="none"
-    document.getElementById("inicio-empleados").style.display= "none"
-    document.getElementById("pedidos-sin-tomar").style.display= "none"
-    document.getElementById("empresas").style.display= "block"
-    document.getElementById("agregar-empresa").style.display= "none"
-    document.getElementById("productos").style.display= "none"
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "block";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "none";
 
   }
 
   agregarEmpresa = () =>{
     document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
     
-    document.getElementById("inicio-sesion").style.display="none"
-    document.getElementById("inicio").style.display="none"
-    document.getElementById("inicio-empleados").style.display= "none"
-    document.getElementById("pedidos-sin-tomar").style.display= "none"
-    document.getElementById("empresas").style.display= "none"
-    document.getElementById("agregar-empresa").style.display= "block"
-    document.getElementById("productos").style.display= "none"
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "block";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "none";
   }
 
   productos =() => {
     document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
     
-    document.getElementById("inicio-sesion").style.display="none"
-    document.getElementById("inicio").style.display="none"
-    document.getElementById("inicio-empleados").style.display= "none"
-    document.getElementById("pedidos-sin-tomar").style.display= "none"
-    document.getElementById("empresas").style.display= "none"
-    document.getElementById("agregar-empresa").style.display= "none"
-    document.getElementById("productos").style.display= "block"
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "block";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "none";
+  }
+
+  productosDisponibles = () => {
+    document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
+    
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "block";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "none";
+  }
+
+  agregarProductos = () => {
+    document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
+    
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "block";
+    document.getElementById("perfil").style.display= "none";
+  }
+
+  perfilAdministrador = () => {
+    document.getElementById("body").style.backgroundImage="linear-gradient(#FFFFFF, #FFFFFF)";
+    
+    document.getElementById("inicio-sesion").style.display="none";
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("inicio-empleados").style.display= "none";
+    document.getElementById("pedidos-sin-tomar").style.display= "none";
+    document.getElementById("empresas").style.display= "none";
+    document.getElementById("agregar-empresa").style.display= "none";
+    document.getElementById("productos").style.display= "none";
+    document.getElementById("productos-Disponibles").style.display= "none";
+    document.getElementById("agregar-Productos").style.display= "none";
+    document.getElementById("perfil").style.display= "block";
   }
